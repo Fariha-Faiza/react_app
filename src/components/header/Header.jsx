315bc161b4fe1a4adf8d06a7878f9db397f54455
Fariha Islam Faiza <fariha.faiza@LEADSOFT.BIZ>
2022-08-10 02:29:50 -0700

    from office pc test

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className="header">
         <div className="headerTitles">
             <span className="headerTitleSm"> Welcome To </span>
-            <span className="headerTitleLg">Mobile legends </span>
+            <span className="headerTitleLg"> React App Practicing  </span>
         </div>
         <img className="headerImg"
          src= "https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" 
